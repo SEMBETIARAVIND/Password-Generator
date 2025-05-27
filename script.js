ca09a8c5800db893d9a72695cb06744d585c3189
SEMBETIARAVIND <aravindsembeti18@gmail.com>
2025-05-27 19:53:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Random Password Generator</title>
-	<link rel="stylesheet" href="style.css">
-</head>
-<body>
-
-<div class="container">
-    <h1>Generate a <br><span>Random Password</span></h1>
-
-    <div class="display">
-        <input type="text" id="password" placeholder="Password" readonly>
-        <button id="copyBtn" class="copy-icon-btn" title="Copy Password">📋</button>
-    </div>
-
-    <button onclick="generatePassword()" class="generate-btn">Generate Password</button>
-
-    <p id="copyFeedback">Password copied!</p>
-</div>
-
-<script src="script.js"></script>
-</body>
-</html>
+function generatePassword() {
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+  let password = "";
+  for (let i = 0; i < 12; i++) {
+    const randomIndex = Math.floor(Math.random() * chars.length);
+    password += chars[randomIndex];
+  }
+  document.getElementById("password").value = password;
+}
